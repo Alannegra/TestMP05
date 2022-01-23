f9c04d5df73aa879d0c4827184faeeebe6e21cad
Alannegra <alanrodriguez@elpuig.xeill.net>
2022-01-23 17:24:12 +0100

    4. Branca1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hola mon");
-        String rebase1 = "text";
-        String rebase2 = "text";
-        String rebase3 = "text";
-        String rebase4 = "text";
-        String rebase5 = "text";
-
+        int branca1 = 1;
     }
 }
