1fd28a02ed2d92793fbc9cedd848d1751dd44323
Alannegra <alanrodriguez@elpuig.xeill.net>
2022-01-23 17:17:37 +0100

    3. Barcelonarebase1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hola mon");
-        String rebase = "text";
+        String rebase1 = "text";
+        String rebase2 = "text";
+        String rebase3 = "text";
+
     }
 }
